04806c9c14aaa9d42daa3a2e7958c569ef28cc85
tsun <tsun@clearpathrobotics.com>
2017-05-15 17:46:27 -0400

    encoders work ish

--- teensy_node/teensy_node.ino
+++ teensy_node/teensy_node.ino
@@ -3,6 +3,7 @@
 #include <std_msgs/Empty.h>
 #include <std_msgs/Int32.h>
 #include <geometry_msgs/Twist.h>
+#include <SoftwareSerial.h>
 
 #define buttonPin 2
 #define ledPin 13
@@ -26,7 +27,7 @@
 #define left_encoder_packet_ID 43
 #define right_encoder_packet_ID 44
 
-#define delay_time  20
+#define delay_time  10
 
 bool fullmode = 0;
 float linearlow = 0.0;
@@ -41,6 +42,9 @@ ros::NodeHandle twist_nh;
 geometry_msgs::Twist debugtwist;
 ros::Publisher twistDebug("/turtledebug/cmd_vel", &debugtwist);
 
+std_msgs::Int32 lastLeftEncod;
+std_msgs::Int32 lastRightEncod;
+
 std_msgs::Int32 leftEncod;
 std_msgs::Int32 rightEncod;
 ros::Publisher leftEncoderPub("/R_left_encode", &leftEncod);
@@ -48,9 +52,9 @@ ros::Publisher rightEncoderPub("/R_right_encode", &rightEncod);
 
 
 void twist_CB(const geometry_msgs::Twist& twistmsg) {
-
   //Serial.println(twistmsg.linear.x);
   roombaDrive(R_DRIVE, twistmsg.linear.x, twistmsg.angular.z);
+  getEncoderData();
 }
 
 void hexSplitForRoomba(int num, int& a1, int& a2) {
@@ -71,11 +75,11 @@ void roombaDrive(int op_code, int lin, int ang) {
   int vel = map(lin, -1, 1, -200, 200);
   int rad = map(ang, -1, 1, -200, 200);
 
-//  //debug----
-//  debugtwist.linear.x = vel;
-//  debugtwist.angular.z = rad;
-//  twistDebug.publish(&debugtwist);
-//  //debug----
+  //  //debug----
+  //  debugtwist.linear.x = vel;
+  //  debugtwist.angular.z = rad;
+  //  twistDebug.publish(&debugtwist);
+  //  //debug----
 
   hexSplitForRoomba(vel, b1, b2);
   hexSplitForRoomba(rad, b3, b4);
@@ -96,20 +100,105 @@ void roombaDrive(int op_code, int lin, int ang) {
 
   //debugWrite(op_code, b1, b2, b3, b4);
 
-  roomba.write(op_code);
-  delay(delay_time);
-  roomba.write(b1);
-  delay(delay_time);
-  roomba.write(b2);
-  delay(delay_time);
-  roomba.write(b3);
-  delay(delay_time);
-  roomba.write(b4);
-  delay(delay_time);
+  roomba.write(byte(op_code));
+  //delay(delay_time);
+  roomba.write(byte(b1));
+  //delay(delay_time);
+  roomba.write(byte(b2));
+  //delay(delay_time);
+  roomba.write(byte(b3));
+  //delay(delay_time);
+  roomba.write(byte(b4));
+  //delay(delay_time);
 }
 
-//ros::Subscriber<geometry_msgs::Twist> twistsub("/cmd_vel_mux/input/teleop", &twist_CB);
-ros::Subscriber<geometry_msgs::Twist> twistsub("/turtle1/cmd_vel", &twist_CB); //for testing with turtle_teleop_key
+void flash() {
+  digitalWrite(ledPin, HIGH);
+  delay(2);
+  digitalWrite(ledPin, LOW);
+  delay(2);
+}
+///////////////////////////////////////
+int getSensorData(byte sensorID)
+{
+  int returnVal;
+  byte packetID = 0;
+
+  byte MSB = 0;
+  byte LSB = 0;
+  roomba.write(142);
+  roomba.write(packetID);
+
+  while (!roomba.available());
+  MSB = roomba.read();
+  LSB = roomba.read();
+  returnVal = (MSB << 7) | LSB;
+
+
+  return returnVal;
+}
+//////////////////////////////////////
+
+void captureEncoderData(std_msgs::Int32& encod) {
+  uint16_t highbyte;
+  uint16_t lowbyte;
+  highbyte = roomba.read();
+  //delay(delay_time);
+  lowbyte = roomba.read();
+  //delay(delay_time);
+
+  encod.data = (int)(highbyte << 0xFF) | (int)(lowbyte & 0xFF);
+}
+
+void startEncodStream() {
+  roomba.write(byte(148));
+  roomba.write(byte(2));
+  roomba.write(byte(left_encoder_packet_ID));
+  roomba.write(byte(right_encoder_packet_ID));
+  delay(100);
+}
+
+void getEncoderData() {
+//  signed char sensorbytes[4];
+//  int data = 1;
+  /*signed 16bit number, high byte first. This number will roll
+    over if it passes the max value at approx 14.5m*/
+//  roomba.write(byte(149)); // request encoder counts
+//  roomba.write(byte(2));
+//  roomba.write(byte(43));
+//  roomba.write(byte(44));
+//  delay(10); // wait for sensors
+//  int i = 0;
+//  while (data == 1) {
+//    sensorbytes[i++] = roomba.read();  //read values into signed char array
+//    if (i == 3) {
+//      data = 0;
+//    }
+//  }
+
+  //merge upper and lower bytes
+//  rightEncod.data = (int)(sensorbytes[2] << 8) + (int)(sensorbytes[3]);
+//  leftEncod.data = (int)(sensorbytes[0] << 8) + (int)(sensorbytes[1]);
+
+  leftEncod.data = getSensorData(43);
+  rightEncod.data = getSensorData(44);
+
+  if (abs(leftEncod.data - lastLeftEncod.data) < 100 || 
+      abs(rightEncod.data - lastRightEncod.data) < 100 ) {
+    lastLeftEncod = leftEncod;
+    lastRightEncod = rightEncod;
+    leftEncoderPub.publish(&leftEncod);
+    rightEncoderPub.publish(&rightEncod);
+      }
+
+ 
+  //  }
+  //  lastLeftEncod = leftEncod;
+  //  lastRightEncod = rightEncod;
+}
+
+ros::Subscriber<geometry_msgs::Twist> twistsub("/cmd_vel_mux/input/teleop", &twist_CB);
+//ros::Subscriber<geometry_msgs::Twist> twistsub("/turtle1/cmd_vel", &twist_CB); //for testing with turtle_teleop_key
 /*************************************************************
   SETUP
 *************************************************************/
@@ -123,68 +212,17 @@ void setup() { // will be triggered when robot is powered on
   // start HW1 serial
   roomba.begin(115200);
   // start HW2 serial
-  //euclid.begin(0);
+
   // start USB serial port
   twist_nh.initNode();
   twist_nh.subscribe(twistsub);
   twist_nh.advertise(twistDebug);
-  //twist_nh.getHardware()->setBaud(115200);
   twist_nh.advertise(leftEncoderPub);
   twist_nh.advertise(rightEncoderPub);
 
-  //Serial.println("Initialize complete");
   delay(500);
   roomba.write(R_RESET); // reset Roomba, to clear all current states
-  //Serial.println("Resetting Roomba");
   flash();
-}
-
-void flash() {
-  digitalWrite(ledPin, HIGH);
-  delay(5);
-  digitalWrite(ledPin, LOW);
-  delay(5);
-}
-
-void captureEncoderData(std_msgs::Int32& encod) {
-  highbyte = roomba.read();
-  delay(delay_time);
-  lowbyte = roomba.read();
-  delay(delay_time);
-
-  encod = (highbyte << 0xFF) | lowbyte;
-}
-
-void getEncoderData() {
-  /*signed 16bit number, high byte first. This number will roll 
-  over if it passes the max value at approx 14.5m*/
-  uint16_t highbyte;
-  uint16_t lowbyte;
-  bool done_read_l = 0;
-  bool done_read_r = 0;
-
-  roomba.write(148);
-  delay(delay_time);
-  roomba.write(2);
-  delay(delay_time);
-  roomba.write(left_encoder_packet_ID);
-  delay(delay_time);
-  roomba.write(right_encoder_packet_ID);
-  delay(delay_time);
-
-  /*while packet id != packet, keep reading.
-   * If packet id == packet, store next value
-   */
-  while(done_read_l == 0 && done_read_r == 0) {
-    if (roomba.read() == left_encoder_packet_ID) {
-      delay(delay_time);
-      captureEncoderData(leftEncod);
-    }
-    else if (roomba.read() == right_encoder_packet_ID) {
-      delay(delay_time);
-      captureEncoderData(rightEncod);
-    }
-  }
 }
 
 /*************************************************************
@@ -220,11 +258,15 @@ void loop() {
     fullmode = 1;
     Serial.flush();
     //Serial.begin(9600);
+    startEncodStream();
+    flash();
+    delay(10);
+    flash();
   }
 
   if (fullmode == 1) {
     //spin other nodes here
     twist_nh.spinOnce();
-    delay(delay_time);
+    delay(10);
   }
 }
